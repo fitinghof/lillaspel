39bde89a73b56d4a02c4acb0eda67f6ef71fe124
DagOhlin <ohlin.dag@gmail.com>
2026-01-27 20:54:21 +0100

    added functionality to log, enum to pick wich type

--- headers/utilities/logger.h
+++ headers/utilities/logger.h
@@ -11,24 +11,77 @@
 #define YELLOW	"\033[33m"
 #define RED		"\033[31m" 
 
+enum LogType
+{
+	consoleAndLogFile,
+	consoleOnly,
+	logFileOnly
+};
+
 class Logger {
 public:
-	static void Log(std::string info) {
-		std::cout << info << std::endl;
+	static void Log(std::string info, LogType logType = LogType::consoleAndLogFile) {
 
-		PrintToFile(info);
-	}
+		switch (logType)
+		{
+		case LogType::consoleAndLogFile:
+			std::cout << info << std::endl;
+			PrintToFile(info);
+			break;
+		case LogType::consoleOnly:
+			std::cout << info << std::endl;
+			break;
+		case LogType::logFileOnly:
+			PrintToFile(info);
+			break;
+		default:
+			break;
+		}
 
-	static void Warn(std::string info) {
-		std::cout << YELLOW << "[WARNING] " << info << RESET_COLOR << std::endl;
 		
-		PrintToFile("[WARNING] " + info);
 	}
 
-	static void Error(std::string info) {
-		std::cout << RED << "[ERROR] " << info << RESET_COLOR << std::endl;
+	static void Warn(std::string info, LogType logType = LogType::consoleAndLogFile) {
 
-		PrintToFile("[ERROR] " + info);
+		switch (logType)
+		{
+		case LogType::consoleAndLogFile:
+			std::cout << YELLOW << "[WARNING] " << info << RESET_COLOR << std::endl;
+			PrintToFile("[WARNING] " + info);
+			break;
+		case LogType::consoleOnly:
+			std::cout << YELLOW << "[WARNING] " << info << RESET_COLOR << std::endl;
+			break;
+		case LogType::logFileOnly:
+			PrintToFile("[WARNING] " + info);
+			break;
+		default:
+			break;
+		}
+		
+		
+	}
+
+	static void Error(std::string info, LogType logType = LogType::consoleAndLogFile) {
+
+		switch (logType)
+		{
+		case LogType::consoleAndLogFile:
+			std::cout << RED << "[ERROR] " << info << RESET_COLOR << std::endl;
+			PrintToFile("[ERROR] " + info);
+			break;
+		case LogType::consoleOnly:
+			std::cout << RED << "[ERROR] " << info << RESET_COLOR << std::endl;
+			break;
+		case LogType::logFileOnly:
+			PrintToFile("[ERROR] " + info);
+			break;
+		default:
+			break;
+		}
+		
+
+		
 	}
 
 	static void PrintToFile(std::string info) {

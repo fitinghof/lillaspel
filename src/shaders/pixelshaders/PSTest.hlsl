edd6085be4d8b70125456d610fe9e9adc95880ab
Exeboss02 <gabriel.abr.wik@outlook.com>
2026-01-27 16:13:57 +0100

    added shader folders and basic shaders

---
+++ src/shaders/pixelshaders/PSTest.hlsl
@@ -0,0 +1,13 @@
+
+struct PixelShaderInput
+{
+    float4 position : SV_POSITION;
+    float4 vertexWorldPosition : VERTEX_WORLD_POSITION;
+    float4 normal : NORMAL;
+    float2 texcord : TEXCORD;
+};
+
+float4 main(PixelShaderInput input) : SV_TARGET
+{	
+    return float4(0, 1, 0, 1);
+}
---
+++ src/shaders/vertexshaders/VSTest.hlsl
@@ -0,0 +1,26 @@
+struct VertexShaderInput
+{
+    float3 position : POSITION;
+    float3 normal : NORMAL;
+    float2 texcord : TEXCORD;
+};
+
+struct VertexShaderOutput
+{
+    float4 position : SV_POSITION;
+    float4 vertexWorldPosition : VERTEX_WORLD_POSITION;
+    float4 normal : NORMAL;
+    float2 texcord : TEXCORD;
+};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    
+    output.position = float4(input.position, 1);
+    output.vertexWorldPosition = float4(input.position, 1);
+    output.normal = float4(input.normal, 1);
+    output.texcord = input.texcord;
+    
+    return output;
+}

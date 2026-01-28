9b70b85b07637a2e2b1f7ab70aeec8b66af9733e
Simon Nilsson <simon.f.nilsson@me.com>
2026-01-28 13:24:08 +0000

    Added objectloader class and started work on LoadGltf function

---
+++ headers/objects/objectLoader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <fastgltf/core.hpp>
+#include <string>
+
+
+class ObjectLoader
+{
+public:
+	ObjectLoader();
+	~ObjectLoader():
+	void LoadGltf(std::string path);
+
+
+
+private:
+	struct ObjectResources {
+		std::vector<
+	};
+}

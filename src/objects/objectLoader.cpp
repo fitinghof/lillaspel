9c246aa93c58835ca8981f1f1b701ca2f9c51b71
Simon Nilsson <simon.f.nilsson@me.com>
2026-01-29 11:03:54 +0000

    Managed to figure out how to extract positions from fastgltf, Intellisens is no good

--- src/objects/objectLoader.cpp
+++ src/objects/objectLoader.cpp
@@ -12,15 +12,36 @@ void ObjectLoader::LoadGltf(std::filesystem::path path) {
 	fastgltf::Parser parser;
 
 	auto gltfFile = fastgltf::GltfDataBuffer::FromPath(path);
-	auto data = parser.loadGltf(gltfFile.get(), path.parent_path());
-
-	if (!data) {
+	if (gltfFile.error() != fastgltf::Error::None) {
+		Logger::Error("Failed to load gltf file");
+		Logger::Error(std::to_string((int)gltfFile.error()));
 		throw std::runtime_error("Failed");
 	}
 
-	for (const auto& mesh : data.get().meshes) {
+	auto data = parser.loadGltf(gltfFile.get(), path.parent_path());
+
+	if (data.error() != fastgltf::Error::None) {
+		Logger::Error("Failed to load glTF file");
+		Logger::Error(std::to_string((int)data.error()));
+		throw std::runtime_error("Failed");
+	}
+
+	std::vector<fastgltf::math::fvec3> positions;
+	auto& asset = data.get();
+	for (const auto& mesh : asset.meshes) {
 		for (const auto& primitive : mesh.primitives) {
-			Logger::Log(primitive.attributes[0].name.c_str());
+			auto* positionIt = primitive.findAttribute("POSITION");
+			auto& positionAccessor = asset.accessors[positionIt->accessorIndex];
+
+			if (!positionAccessor.bufferViewIndex.has_value())
+				continue;
+
+			positions.reserve(positionAccessor.count);
+			fastgltf::iterateAccessor<fastgltf::math::fvec3>(asset, positionAccessor, [&](fastgltf::math::fvec3 position) {
+				positions.emplace_back(position);
+			});
+
+
 		}
 	}
 }

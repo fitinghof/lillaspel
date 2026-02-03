1c17abedd26ec0dd5cca5132b835248451c2d54f
Exeboss02 <gabriel.abr.wik@outlook.com>
2026-02-03 17:28:39 +0100

    added notes

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -15,29 +15,6 @@ void Game::Run(HINSTANCE hInstance, int nCmdShow) {
 
     MSG msg = {};
 
-    MusicTrackManager mtm;
-    mtm.Initialize("C:/Users/Gabriel/Music/Testing/");
-    mtm.AddMusicTrackStandardFolder("Sneak16.wav", "sneak");
-    mtm.AddMusicTrackStandardFolder("BloodyFangs16.wav", "fangs");
-
-    mtm.SetGain("sneak", 0.8f);
-    mtm.FadeInPlay("sneak", 0, 12);
-
-    SoundBank sb;
-    sb.Initialize("C:/Users/Gabriel/Music/Testing/");
-    sb.AddSoundClipStandardFolder("TestSound.wav", "scream1");
-
-    SoundClip* clip = sb.GetSoundClip("scream1");
-
-    SoundSourceObject sso;
-    sso.SetRandomPitch(1.2f, 1.8f);
-
-    float time = 3;
-    float timer = time;
-
-    SetListenerPosition(-5, 0, -1);
-
-
     while (msg.message != WM_QUIT)
     {
         this->imguiManager.ImguiAtFrameStart();
@@ -45,17 +22,6 @@ void Game::Run(HINSTANCE hInstance, int nCmdShow) {
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
-        }
-
-        timer -= Time::GetInstance().GetDeltaTime();
-
-        mtm.Tick();
-
-        if (timer <= 0)
-        {
-            timer = time;
-            //mtm.Stop("fangs");
-            sso.Play(clip);
         }
 
         Time::GetInstance().Tick();

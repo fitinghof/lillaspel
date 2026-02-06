f2705f994643cb3b57cbed3c69235cd53908dc3a
Dag Ohlin <ohlin.dag@gmail.com>
2026-02-06 15:54:16 +0100

    added the code atleast

--- src/gameObjects/debugCamera.cpp
+++ src/gameObjects/debugCamera.cpp
@@ -6,6 +6,16 @@ void DebugCamera::Tick()
 
 	// Until we get input
 
+
+	int centerX = 500;
+	int centerY = 500;
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	// Logic to calculate delta and rotate
+	
+	
+
+
 	//static float pos[3] = { 0,0,0 };
 	static float rot[3] = { 0,0,0 };
 
@@ -17,8 +27,8 @@ void DebugCamera::Tick()
 	float sensitivity = 2.0f;
 	float rotSpeed = sensitivity * Time::GetInstance().GetDeltaTime();
 
-	rot[0] += rightStick.at(1) * rotSpeed * -1.0f;
-	rot[1] += rightStick.at(0) * rotSpeed;
+	rot[0] += rightStick.at(1) * rotSpeed * -1.0f * (cursorPos.y - centerY);
+	rot[1] += rightStick.at(0) * rotSpeed * (cursorPos.x - centerX);
 
 	if (rot[0] > 1.5f) rot[0] = 1.5f;
 	if (rot[0] < -1.5f) rot[0] = -1.5f;
@@ -27,4 +37,6 @@ void DebugCamera::Tick()
 	this->transform.SetRotationRPY(0.0f, rot[0], rot[1]);
 
 	this->transform.Move(this->transform.GetDirectionVector(), Time::GetInstance().GetDeltaTime() * leftStick.at(1) * 20);
+
+	SetCursorPos(centerX, centerY);
 }

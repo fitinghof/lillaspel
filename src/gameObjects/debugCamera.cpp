27ce3ef04394d3b899d84a7f1b01ab94cde61651
Simon Nilsson <simon.f.nilsson@me.com>
2026-02-10 13:47:14 +0000

    removed logging

--- src/gameObjects/debugCamera.cpp
+++ src/gameObjects/debugCamera.cpp
@@ -19,42 +19,9 @@ void DebugCamera::Tick()
 		PostQuitMessage(0);
 	}
 
-	if (keyboardInput.Jump()) {
-		Logger::Log("Jump pressed");
-	}
-	
-	std::array<float, 2> movementVector = keyboardInput.GetMovementVector();
-	if (movementVector[0] != 0.0f || movementVector[1] != 0.0f) {
-		Logger::Log("Movement vector: " + std::to_string(movementVector[0]) + ", " + std::to_string(movementVector[1]));
-	}
-
-	std::array<float, 2> lookVector = keyboardInput.GetLookVector();
-	if (lookVector[0] != 0.0f || lookVector[1] != 0.0f) {
-		Logger::Log("Look vector: " + std::to_string(lookVector[0]) + ", " + std::to_string(lookVector[1]));
-	}
-
-
 	InputManager::GetInstance().ReadControllerInput(this->controllerInput.GetControllerIndex());
 
 	if (this->controllerInput.Quit()) {
 		PostQuitMessage(0);
-	}
-
-	if (this->controllerInput.Jump()) {
-		Logger::Log("Controller Jump pressed");
-	}
-
-	if (this->controllerInput.LeftClick()) {
-		Logger::Log("Controller Left Click pressed");
-	}
-	
-	movementVector = this->controllerInput.GetMovementVector();
-	if (movementVector[0] != 0.0f || movementVector[1] != 0.0f) {
-		Logger::Log("Controller Movement vector: " + std::to_string(movementVector[0]) + ", " + std::to_string(movementVector[1]));
-	}
-
-	lookVector = this->controllerInput.GetLookVector();
-	if (lookVector[0] != 0.0f || lookVector[1] != 0.0f) {
-		Logger::Log("Controller Look vector: " + std::to_string(lookVector[0]) + ", " + std::to_string(lookVector[1]));
 	}
 }

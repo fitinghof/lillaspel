8e6f7cda194d837780e57e2ad0058b550c8cc209
OuterFrog <91077751+OuterFrog@users.noreply.github.com>
2026-02-11 16:58:44 +0100

    Now loads a dds texture cube

--- headers/rendering/textureCube.h
+++ headers/rendering/textureCube.h
@@ -1,30 +1,22 @@
 #pragma once
 
 #include <d3d11.h>
+#include <DDSTextureLoader.h>
 
 #include "rendering/renderTarget.h"
-#include "rendering/i
 
 class TextureCube {
 private:
-	Microsoft::WRL::ComPtr<ID3D11Texture2D> textures = nullptr;
-	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> rtvs[6] {nullptr};
+	Microsoft::WRL::ComPtr<ID3D11Resource> texture;
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> shaderResourceView;
 
-	void CreateTextures(ID3D11Device* device, UINT width, UINT height);
-	void CreateTextureRTVs(ID3D11Device* device);
-	void CreateTextureSRV(ID3D11Device* device);
-
-	Vector2 resolution;
+	void CreateTextures(ID3D11Device* device, ID3D11DeviceContext* deviceContext, std::string filepath);
 
 public:
 	TextureCube() = default;
 	~TextureCube() = default;
 
-	void Init(ID3D11Device* device, UINT width, UINT height);
+	void Init(ID3D11Device* device, UINT width, UINT height) {};
 
-	ID3D11RenderTargetView* GetRTV(size_t index);
-	ID3D11ShaderResourceView* GetSRV() const;
-
-	//Vector2 GetResolution() const;
+	ID3D11ShaderResourceView* GetSRV() const { return nullptr; };
 };
--- src/rendering/textureCube.cpp
+++ src/rendering/textureCube.cpp
@@ -1,21 +1,10 @@
-#include "textureCube.h"
+#include "rendering/textureCube.h"
 
-void TextureCube::CreateTextures(ID3D11Device* device, UINT width, UINT height) 
+void TextureCube::CreateTextures(ID3D11Device* device, ID3D11DeviceContext* deviceContext, std::string filepath) 
 {
-	D3D11_TEXTURE2D_DESC desc;
-	desc.Width = width;
-	desc.Height = height;
-	desc.MipLevels = 1;
-	desc.ArraySize = 6;
-	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; 
-	desc.SampleDesc = {1, 0};				  // Create new sampleDesc with Count = 1, Quality = 0
-	desc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
-	desc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
-	desc.CPUAccessFlags = 0;
-	desc.MiscFlags = D3D11_RESOURCE_MISC_TEXTURECUBE;
-
-	HRESULT hr = device->CreateTexture2D(&desc, nullptr, &this->textures);
-	if (FAILED(hr)) {
-		throw std::exception("Failed to create texture cube!");
-	}
+	const std::wstring wstr = std::wstring(filepath.begin(), filepath.end());
+	DirectX::CreateDDSTextureFromFileEx(device, deviceContext, wstr.c_str(), 0,
+										D3D11_USAGE_DEFAULT,
+										D3D11_BIND_SHADER_RESOURCE, 0, D3D11_RESOURCE_MISC_TEXTURECUBE, DirectX::DX11::DDS_LOADER_DEFAULT,
+										this->texture.GetAddressOf(), this->shaderResourceView.GetAddressOf());
 }

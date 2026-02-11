a2c8098a3c7c19ce8e729f893de0ad134bf71358
Exeboss02 <gabriel.abr.wik@outlook.com>
2026-02-11 15:56:10 +0100

    started debugging boxVSbox collision

--- src/scene/sceneManager.cpp
+++ src/scene/sceneManager.cpp
@@ -58,103 +58,6 @@ void SceneManager::LoadScene(Scenes scene)
 	default:
 		break;
 	}
-
-	//Temp
-
-	CreateNewScene(this->mainScene);
-
-	auto cam = this->mainScene->CreateGameObjectOfType<CameraObject>();
-
-	//////////////////////////////////////////////////
-
-	// std::shared_ptr<MeshObject> mesh = this->mainScene->CreateGameObjectOfType<MeshObject>().lock();
-	// std::shared_ptr<TestPlayer> player = this->mainScene->CreateGameObjectOfType<TestPlayer>().lock();
-	// mesh->SetParent(player);
-
-	// //////////////////////////////////////////////////
-
-	// DirectX::XMVECTOR pos1;
-	// pos1.m128_f32[0] = -3.0f;
-	// pos1.m128_f32[1] = 0;
-	// pos1.m128_f32[2] = 0;
-
-	// DirectX::XMVECTOR pos2;
-	// pos2.m128_f32[0] = 2;
-	// pos2.m128_f32[1] = 0;
-	// pos2.m128_f32[2] = 0;
-
-	// auto coll1 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-	// auto coll2 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-
-	// std::shared_ptr<BoxCollider> Coll1 = coll1.lock();
-	// Coll1->solid = true;
-	// Coll1->dynamic = true;
-	// Coll1->SetPosition(DirectX::XMFLOAT3(4, 0, 0));
-	// Coll1->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-
-	// std::shared_ptr<BoxCollider> Coll2 = coll2.lock();
-	// Coll2->solid = true;
-	// Coll2->dynamic = true;
-	// Coll2->SetPosition(DirectX::XMFLOAT3(2.5f, 0, 0));
-	// Coll2->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-
-	///////////////////////////////////////////////////////////////////
-
-	//auto coll1 = this->mainScene->CreateGameObjectOfType<SphereCollider>();
-	//auto coll2 = this->mainScene->CreateGameObjectOfType<SphereCollider>();
-
-	//std::shared_ptr<SphereCollider> Coll1 = coll1.lock();
-	//Coll1->solid = true;
-	//Coll1->dynamic = true;
-	//Coll1->SetPosition(DirectX::XMFLOAT3(4, 0, 0));
-	//Coll1->SetDiameter(1);
-
-	//std::shared_ptr<SphereCollider> Coll2 = coll2.lock();
-	//Coll2->solid = true;
-	//Coll2->dynamic = true;
-	//Coll2->SetPosition(DirectX::XMFLOAT3(3.4f, 0, 0));
-	//Coll2->SetDiameter(1);
-
-	//Logger::Log("sphere1 center: " + std::to_string(Coll1->GetGlobalPosition().m128_f32[0]) + ", " + std::to_string(Coll1->GetGlobalPosition().m128_f32[1]) + ", " + std::to_string(Coll1->GetGlobalPosition().m128_f32[2]));
-	//Logger::Log("sphere2 center: " + std::to_string(Coll2->GetGlobalPosition().m128_f32[0]) + ", " + std::to_string(Coll2->GetGlobalPosition().m128_f32[1]) + ", " + std::to_string(Coll2->GetGlobalPosition().m128_f32[2]));
-
-	// if (Coll1->Collision(Coll2.get())) Logger::Log("true");
-	// else Logger::Log("false");
-
-	// Coll1->Tick();
-	// Coll2->Tick();
-
-	// Logger::Log("*********** END ************");
-
-	// if (Coll1->Collision(Coll2.get())) Logger::Log("true");
-	// else Logger::Log("false");
-
-	// Coll1->Tick();
-	// Coll2->Tick();
-
-	// Logger::Log("*********** END ************");
-
-	//Logger::Log("sphere1 center: " + std::to_string(Coll1->GetGlobalPosition().m128_f32[0]) + ", " + std::to_string(Coll1->GetGlobalPosition().m128_f32[1]) + ", " + std::to_string(Coll1->GetGlobalPosition().m128_f32[2]));
-	//Logger::Log("sphere2 center: " + std::to_string(Coll2->GetGlobalPosition().m128_f32[0]) + ", " + std::to_string(Coll2->GetGlobalPosition().m128_f32[1]) + ", " + std::to_string(Coll2->GetGlobalPosition().m128_f32[2]));
-
-	////////////////////////////////////////////////////////
-
-	//auto rb1 = this->mainScene->CreateGameObjectOfType<RigidBody>();
-	//auto coll1 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-	//coll1.lock()->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-	//coll1.lock()->SetParent(rb1);
-	//coll1.lock()->SetPosition(DirectX::XMFLOAT3(0, 0, 0)); //to make sure cornersArray is rebuilt
-	//rb1.lock()->transform.SetPosition(pos1);
-
-	//auto rb2 = this->mainScene->CreateGameObjectOfType<RigidBody>();
-	//auto coll2 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-	//coll2.lock()->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-	//coll2.lock()->SetParent(rb2);
-	//coll2.lock()->SetPosition(DirectX::XMFLOAT3(0, 0, 0));
-	//rb2.lock()->transform.SetPosition(pos2);
-
-	//if (rb1.lock()->Collision(rb2)) Logger::Log("true");
-	//else Logger::Log("false");
 }
 
 void SceneManager::CreateNewScene(std::shared_ptr<Scene>& scene)
@@ -239,31 +142,6 @@ void SceneManager::SaveSceneToFile(const std::string &filePath)
 	//Logger::Log("Loaded scene");
 
 	////////////////
-
-	DirectX::XMVECTOR pos1;
-	pos1.m128_f32[0] = -2.0f;
-	pos1.m128_f32[1] = 0;
-	pos1.m128_f32[2] = 0;
-
-	DirectX::XMVECTOR pos2;
-	pos2.m128_f32[0] = 2;
-	pos2.m128_f32[1] = 0;
-	pos2.m128_f32[2] = 0;
-
-	auto rb1 = this->mainScene->CreateGameObjectOfType<RigidBody>();
-	auto coll1 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-	coll1.lock()->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-	coll1.lock()->SetParent(rb1);
-	rb1.lock()->transform.SetPosition(pos1);
-
-	auto rb2 = this->mainScene->CreateGameObjectOfType<RigidBody>();
-	auto coll2 = this->mainScene->CreateGameObjectOfType<BoxCollider>();
-	coll2.lock()->SetExtents(DirectX::XMFLOAT3(1, 1, 1));
-	coll2.lock()->SetParent(rb2);
-	rb2.lock()->transform.SetPosition(pos2);
-
-	if(rb1.lock()->Collision(rb2)) Logger::Log("true");
-	else Logger::Log("false");
 	std::ofstream outFile(filePath);
 	outFile << data;
 	outFile.close();

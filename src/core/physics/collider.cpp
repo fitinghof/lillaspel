f8d5e02f46f919b7c0ae3b1ddd6477e48297c5c6
Exeboss02 <gabriel.abr.wik@outlook.com>
2026-02-13 11:59:28 +0100

    forcing collider sphere mesh scale to 0.5 to match collider size

--- src/core/physics/collider.cpp
+++ src/core/physics/collider.cpp
@@ -58,8 +58,14 @@ void Collider::Start()
 	GameObject3D::Start();
 
 	MeshObjData meshData = {};
+	DirectX::XMVECTOR scale;
+	scale.m128_f32[0] = 1;
+	scale.m128_f32[1] = 1;
+	scale.m128_f32[2] = 1;
+
 	if(this->type == ColliderType::SPHERE)
 	{
+		scale = DirectX::XMVectorScale(scale, 0.5f);
 		meshData = AssetManager::GetInstance().GetMeshObjData("meshes/indicatorSphere05.glb:Mesh_0");
 	}
 	else
@@ -77,6 +83,7 @@ void Collider::Start()
 	meshData.SetMaterial(0, material.lock());
 	auto visualMeshObject = this->factory->CreateGameObjectOfType<MeshObject>().lock();
 	visualMeshObject->SetMesh(meshData);
+	visualMeshObject->transform.SetScale(scale);
 	visualMeshObject->SetParent(std::static_pointer_cast<Collider>(this->GetPtr()));
 }
 

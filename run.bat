e7f1c6788ab9750ac88e15b8fe6c8beeb1bf4a4a
Simon Nilsson <simon.f.nilsson@me.com>
2026-02-13 12:08:05 +0000

    fixed run file

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 if exist "out\build\x64-debug\Debug\lillaspel.exe" (
-    call "out\build\x64-debug\Debug\lillaspel.exe"
+    cd "out\build\x64-debug\Debug\"
+    call "lillaspel.exe"
 ) else (
     echo executable file didn't exist, build first
 )

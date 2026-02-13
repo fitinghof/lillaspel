19487e8b0d820f3826528622746e5bf708e8bc99
Simon Nilsson <simon.f.nilsson@me.com>
2026-02-13 10:57:48 +0000

    Better build / run

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
 @echo off
-:: Create a build directory
-:: cd build
-:: cmake config CMakeLists.txt
-:: cmake .. -A Win32 || exit /b %errorlevel%
-:: build binary app
+
+:: Rename old exe to ensure that if build fails, the old exe wont be run and misinterpreted as a new build
+if exist "out\build\x64-debug\Debug\lillaspel.exe" (
+    move "out\build\x64-debug\Debug\lillaspel.exe" "out\build\x64-debug\Debug\lillaspel_old.exe"
+)
+
+:: Make the preset for the project
 call cmake --preset x64-debug || exit /b %errorlevel%
 
+:: Build new exe
 call cmake --build --preset x64-debug || exit /b %errorlevel%
--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 @echo off
-cd out\build\x64-debug\Debug
-lillaspel
+if exist "out\build\x64-debug\Debug\lillaspel.exe" (
+    call "out\build\x64-debug\Debug\lillaspel.exe"
+) else (
+    echo executable file didn't exist, build first
+)

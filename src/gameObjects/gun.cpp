fca90e022ae7b40bd26d9434371338dd95c6f421
DagOhlin <ohlin.dag@gmail.com>
2026-02-26 15:34:58 +0000

    fixed

--- src/gameObjects/gun.cpp
+++ src/gameObjects/gun.cpp
@@ -37,7 +37,7 @@ void Gun::Shoot() {
 	Ray ray{Vector3D{posVec}, Vector3D{lookVec}};
 	RayCastData rayCastData;
 
-	bool didHit = PhysicsQueue::GetInstance().castRay(ray, rayCastData);
+	bool didHit = PhysicsQueue::GetInstance().castRay(ray, rayCastData, ~Tag::NOIGNORE, Tag::PLAYER);
 	std::string hitString;
 	if (didHit) {
 

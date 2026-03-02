6e96d070d370145097e20eee41e7fbaa3fdac8bf
DagOhlin <ohlin.dag@gmail.com>
2026-03-02 16:27:16 +0000

    added coliders for solid walls

--- src/gameObjects/wall.cpp
+++ src/gameObjects/wall.cpp
@@ -68,7 +68,9 @@ void Wall::SpawnWallColliders(int wallStateIndex) {
 	this->wallColliders.clear();
 	Room::WallState wallState = static_cast<Room::WallState>(wallStateIndex);
 	switch (wallState) {
-	case (Room::WallState::window || Room::WallState::solid): {
+	case (Room::WallState::window):
+	case (Room::WallState::solid):
+	{
 		auto colliderobj = this->factory->CreateStaticGameObject<BoxCollider>();
 		DirectX::XMFLOAT3 pos(0.0f, 3.0f, 4.75f);
 		colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
@@ -79,6 +81,7 @@ void Wall::SpawnWallColliders(int wallStateIndex) {
 
 		this->wallColliders.push_back(colliderobj);
 	} break;
+
 
 	case (Room::WallState::door): {
 		auto colliderobj = this->factory->CreateStaticGameObject<BoxCollider>();

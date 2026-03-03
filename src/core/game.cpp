1c6b381f77a50d060a1b563452098762ab8c28de
OuterFrog <91077751+OuterFrog@users.noreply.github.com>
2026-03-03 08:39:25 +0100

    main menu

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -46,7 +46,7 @@ void Game::Run(HINSTANCE hInstance, int nCmdShow) {
 
 	this->imguiManager.SetSkyboxPopupCallback([&]() { this->sceneManager->SkyboxMenu(); });
 
-	this->sceneManager->LoadScene(SceneManager::Scenes::GAME);
+	this->sceneManager->LoadScene(SceneManager::Scenes::MAIN_MENU);
 
 	MSG msg = {};
 
